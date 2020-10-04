b73575433970959ff4df2d36af87e156a7df75fb
skymiles <wmz1991@hotmail.com>
2020-10-04 15:09:08 +0800

    96. Unique Binary Search Trees

--- src/com/alibaba/cainiao/leetcode/LeetCode96.java
+++ src/com/alibaba/cainiao/leetcode/LeetCode96.java
@@ -4,25 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 95. Unique Binary Search Trees II
+ * 96. Unique Binary Search Trees
  * Medium
  *
+ * 3811
  *
- * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+ * 137
+ *
+ * Add to List
+ *
+ * Share
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
  *
  * Example:
  *
  * Input: 3
- * Output:
- * [
- *   [1,null,3,2],
- *   [3,2,null,1],
- *   [3,1,null,null,2],
- *   [2,1,3],
- *   [1,null,2,null,3]
- * ]
+ * Output: 5
  * Explanation:
- * The above output corresponds to the 5 unique BST's shown below:
+ * Given n = 3, there are a total of 5 unique BST's:
  *
  *    1         3     3      2      1
  *     \       /     /      / \      \
@@ -33,43 +32,42 @@ import java.util.List;
  *
  * Constraints:
  *
- * 0 <= n <= 8
+ * 1 <= n <= 19
  */
-public class LeetCode95 {
+public class LeetCode96 {
 
-    public List<TreeNode> generateTrees(int n) {
+    public int numTrees(int n) {
+        return generateTrees(n).size();
+    }
+
+    private List<TreeNode> generateTrees(int n) {
         if (n == 0) {
             return new ArrayList<>();
         }
 
-        //从1作为root开始，到n作为root结束
         return dfs(1, n);
     }
 
     private List<TreeNode> dfs(int left, int right) {
-        List<TreeNode> list = new ArrayList<>();
+        List<TreeNode> res = new ArrayList<>();
         if (left > right) {
-            list.add(null);
-            return list;
+            res.add(null);
+            return res;
         }
 
         // 以r为根节点的树，其左子树由[1, i-1]构成， 其右子树由[i+1, n]构成。
         for (int r = left; r <= right; r++) {
             List<TreeNode> leftList = dfs(left, r - 1);
             List<TreeNode> rightList = dfs(r + 1, right);
-
-            // 乘法原理
             for (int i = 0; i < leftList.size(); i++) {
                 for (int j = 0; j < rightList.size(); j++) {
                     TreeNode root = new TreeNode(r);
                     root.left = leftList.get(i);
                     root.right = rightList.get(j);
-                    list.add(root);
+                    res.add(root);
                 }
             }
         }
-
-        return list;
+        return res;
     }
-
 }
