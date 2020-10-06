42c9f0ef64a57a5d0899f0acf219b19dc4743dbd
skymiles <wmz1991@hotmail.com>
2020-10-06 13:18:49 +0800

    maxPathSum

--- src/com/alibaba/cainiao/leetcode/LeetCode124.java
+++ src/com/alibaba/cainiao/leetcode/LeetCode124.java
@@ -31,34 +31,64 @@ package com.alibaba.cainiao.leetcode;
  */
 public class LeetCode124 {
 
-    // https://www.bilibili.com/video/BV19t411w7Ep   必看解析
+//    // https://www.bilibili.com/video/BV19t411w7Ep   必看解析
+//
+//    int res = Integer.MIN_VALUE;
+//
+//    public int maxPathSum(TreeNode root) {
+//        if (root == null) {
+//            return 0;
+//        }
+//        dfs(root);
+//        return res;
+//    }
+//
+//    // 1.返回必须经过root点的节点和最大值 2.最少使用左右一边的子节点,不然不是path
+//    private int dfs(TreeNode root) {
+//        if (root == null) {
+//            return 0;
+//        }
+//
+//        //左边右边要结果
+//        int leftNodeSum = dfs(root.left);
+//        int rightNodeSum = dfs(root.right);
+//
+//        // 最大值是左边的最大值+右边的最大值+root的值
+//        res = Math.max(res, leftNodeSum + rightNodeSum + root.val);
+//
+//        // 1.向左走节点最大值. 2. 向右走节点最大值 3.不走节点最大值
+//        // return 单边
+//        return Math.max(0, root.val + Math.max(0, Math.max(leftNodeSum, rightNodeSum)));
+//    }
 
+    // https://www.bilibili.com/video/BV19t411w7Ep   必看解析
     int res = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
         if (root == null) {
             return 0;
         }
+
         dfs(root);
         return res;
     }
 
-    // 1.返回必须经过root点的节点和最大值 2.最少使用左右一边的子节点,不然不是path
+    // 1. 经过root节点(root是最高点的path)
+    // 2.最多使用左右一边的子节点,不然不是path
     private int dfs(TreeNode root) {
         if (root == null) {
             return 0;
         }
 
         //左边右边要结果
-        int leftNodeSum = dfs(root.left);
-        int rightNodeSum = dfs(root.right);
+        int leftSum = dfs(root.left);
+        int rightSum = dfs(root.right);
 
-        // 最大值是左边的最大值+右边的最大值+root的值
-        res = Math.max(res, leftNodeSum + rightNodeSum + root.val);
+        // 最大path=最大值是左边的最大值+右边的最大值+root的值
+        res = Math.max(res, leftSum + rightSum + root.val);
 
         // 1.向左走节点最大值. 2. 向右走节点最大值 3.不走节点最大值
         // return 单边
-        return Math.max(0, root.val + Math.max(0, Math.max(leftNodeSum, rightNodeSum)));
+        return Math.max(0, root.val + Math.max(0, Math.max(leftSum, rightSum)));
     }
-
 }
