e44fb9d8b29921a0bd50a5ed8faf14b98042c741
skymilesHK <wangmianzhi@connect.hku.hk>
2022-07-03 15:55:10 +0800

    前缀和

--- src/com/alibaba/cainiao/acwing/前缀和.java
+++ src/com/alibaba/cainiao/acwing/前缀和.java
@@ -8,18 +8,28 @@ public class 前缀和 {
     // 前缀和: S[i] = a[1] + a[2] + a[3] + … + a[i]
     static int N = 100009;
     static Scanner in = new Scanner(System.in);
+    static int[] s = new int[N];
+    static int[] a = new int[N];
 
     public static void main(String[] args) {
         int n = in.nextInt();
         int m = in.nextInt();
 
-        int[] a = new int[N];
         // 注意这里是从 1开始的
         for (int i = 1; i <= n; i++) {
             a[i] = in.nextInt();
         }
 
+        for (int i = 1; i <= n; i++) {
+            s[i] = s[i - 1] + a[i];
+        }
 
+        do {
+            int l = in.nextInt();
+            int r = in.nextInt();
+
+            System.out.println(s[r] - s[l - 1]);
+        } while (--m > 0);
     }
 
 }
