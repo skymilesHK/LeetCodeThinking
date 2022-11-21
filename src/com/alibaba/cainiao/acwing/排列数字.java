117eda0fc0e8aea06a07454b6268812e99273c98
skymilesHK <wangmianzhi@connect.hku.hk>
2022-11-21 09:14:18 +0800

    排列数字

--- src/com/alibaba/cainiao/acwing/排列数字.java
+++ src/com/alibaba/cainiao/acwing/排列数字.java
@@ -1,4 +1,4 @@
-package com.alibaba.cainiao;
+package com.alibaba.cainiao.acwing;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,7 +6,12 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Main {
+public class 排列数字 {
+
+    // https://www.bilibili.com/video/BV1M4411Q7td/?spm_id_from=333.999.0.0&vd_source=19a205b524ff07cbc62a6f21c0c6b7c2  先看这个讲解
+    // https://www.acwing.com/solution/content/4340/ 再看这个
+    // https://www.acwing.com/solution/content/4775/  https://www.acwing.com/solution/content/4777/ 这两个是看变量命名
+    // https://www.youtube.com/watch?v=w4SjNXKLsv4 参考
 
     // assume no concurrency
     static List<List<Integer>> res = new ArrayList<>();
@@ -62,5 +67,3 @@ public class Main {
         }
     }
 }
-
-
--- src/com/alibaba/cainiao/acwing/食物链.java
+++ src/com/alibaba/cainiao/acwing/食物链.java
@@ -4,54 +4,64 @@ import java.util.Scanner;
 
 public class 食物链 {
 
-    static int N = 50009;
-    static int n, m, res;
+    static int N = 50002;
+    static int n, k, res;
     static int[] parent = new int[N];
     static int[] dist = new int[N];
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         n = in.nextInt();
-        m = in.nextInt();
-
+        k = in.nextInt();
         for (int i = 1; i <= n; i++) {
             parent[i] = i;
         }
 
         do {
-            int t, x, y;
-            t = in.nextInt();
-            x = in.nextInt();
-            y = in.nextInt();
+            // d--> 说法种类
+            // x--> 吃的物
+            // y--> 被吃的物
+            int d = in.nextInt();
+            int x = in.nextInt();
+            int y = in.nextInt();
+
             if (x > n || y > n) {
                 res++;
             } else {
-                int rooX = find(x);
-                int rooY = find(y);
-                if (t == 1) {
-                    // 同类
-
+                // 先找到父节点
+                int pRoot = find(x);
+                int qRoot = find(y);
+                // 同类
+                if (d == 1) {
+                    if (pRoot == qRoot && (dist[y] - dist[x]) % 3 != 0) {
+                        res++;
+                    } else if (pRoot != qRoot) {
+                        // 当前不在同一集合中，无法判定为假。故为真，应加入同一集合表示存在同类关系
+                        parent[pRoot] = qRoot;
+                        dist[pRoot] = dist[y] - dist[x];
+                    }
                 } else {
-                    // 吃
+                    // 为吃被吃掉的关系
+                    if (pRoot == qRoot && (dist[x] - dist[y] - 1) % 3 != 0) {
+                        res++;
+                    } else if (pRoot != qRoot) {
+                        // 如果不在同一集合内，并到一起，更新距离
+                        parent[pRoot] = qRoot;
+                        dist[pRoot] = dist[y] + 1 - dist[x];
+                    }
                 }
             }
-        } while (--m > 0);
+        } while (--k > 0);
+
+        System.out.println(res);
     }
 
-    static int find(int p) {
+    private static int find(int p) {
         if (p != parent[p]) {
-            int t = find(parent[p]);                // 先把p的父节点及以上压缩到树根
-            dist[p] = dist[p] + dist[parent[p]];    // 事实上，d[x]始终代表到父节点的距离，只不过在find之后x的父节点距离直接变成了到祖宗的距离
-            parent[p] = t;                          // p节点也压缩到树根
+            int r = find(parent[p]);                //先把父节点及以上压缩到树根
+            dist[p] = dist[p] + dist[parent[p]];    //更新边权
+            parent[p] = r;                          //父节点也压缩到树根
         }
         return parent[p];
-    }
-
-    static void union(int p, int q) {
-        int rooP = find(p);
-        int rooQ = find(q);
-        if (rooP == rooQ) {
-
-        }
     }
 }
