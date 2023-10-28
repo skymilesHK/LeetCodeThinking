7035599249d9db7406070505160ecbfb44e29a44
skymilesHK <wangmianzhi@connect.hku.hk>
2023-10-28 15:27:54 +0800

    机器分配

--- src/com/alibaba/cainiao/Main.java
+++ src/com/alibaba/cainiao/Main.java
@@ -4,31 +4,6 @@ import java.util.*;
 
 public class Main {
 
-    // https://www.acwing.com/solution/content/52967/
-    static int N = 5, M = 1002;
-    static int n = 4, m = 0;
-    static int[] v = {0, 10, 20, 50, 100};
-    static int[][] dp = new int[N][M];
-    static Scanner in = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        // m元钱全部用来买书，书的价格为10元，20元，50元，100元
-        m = in.nextInt();
-
-        //dp
-        dp[0][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= v[i]) {
-                    dp[i][j] += dp[i][j - v[i]];
-                }
-            }
-        }
-
-        System.out.println(dp[n][m]);
-    }
-
 }
 
 
